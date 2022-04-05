40935e980d98f2673d4ab0a721a4c0e8d534e955
Ayon <ayonjodder177@gmail.com>
2022-04-05 21:59:55 +0600

    Contact page responsive done

--- src/Components/ComingSoon/CountdownTimer.js
+++ src/Components/ComingSoon/CountdownTimer.js
@@ -24,18 +24,18 @@ const CountdownTimer = ({ countdownTimestampMs }) => {
     }
 
     return (
-        <div className="countdown-timer grid grid-cols-4 gap-4">
-            <div className="box-parent flex items-center justify-center h-28 w-52">
-                <p className='text-3xl font-bold single-timer-box'>{remainingTime.days} Days</p>
+        <div className="countdown-timer grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-4">
+            <div className="box-parent flex items-center justify-center h-16 w-24 md:h-28 md:w-52">
+                <p className='md:text-3xl font-bold single-timer-box'>{remainingTime.days} Days</p>
             </div>
-            <div className="box-parent flex items-center justify-center">
-                <p className='text-3xl font-bold single-timer-box'>{remainingTime.hours} Hours</p>
+            <div className="box-parent flex items-center justify-center h-16 w-24 md:h-28 md:w-52">
+                <p className='md:text-3xl font-bold single-timer-box'>{remainingTime.hours} Hours</p>
             </div>
-            <div className="box-parent flex items-center justify-center">
-                <p className='text-3xl font-bold single-timer-box'>{remainingTime.minutes} Minutes</p>
+            <div className="box-parent flex items-center justify-center h-16 w-24 md:h-28 md:w-52">
+                <p className='md:text-3xl font-bold single-timer-box'>{remainingTime.minutes} Minutes</p>
             </div>
-            <div className="box-parent flex items-center justify-center">
-                <p className='text-3xl font-bold single-timer-box'>{remainingTime.seconds} Seconds</p>
+            <div className="box-parent flex items-center justify-center h-16 w-24 md:h-28 md:w-52">
+                <p className='md:text-3xl font-bold single-timer-box'>{remainingTime.seconds} Seconds</p>
             </div>
         </div>
     );
